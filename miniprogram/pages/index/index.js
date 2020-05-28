025b6697d41f516790da2fcdee84144f8b0638ee
moreant <moreant@foxmail.com>
2020-05-28 08:53:08 +0800

    feat: 1/4

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,72 +1,111 @@
+
 const db = wx.cloud.database()
+const oStudents = db.collection("old_students")
+const lovs = db.collection("loves")
 const $ = db.command.aggregate
 
 Page({
+
   data: {
-    tab: 0,
+
   },
 
-  // 页面切换
-  changeItem: function (e) {
-    this.setData({
-      item: e.target.dataset.item
-    })
-  },
-  // tab切换
-  changeTab: function (e) {
-    this.setData({
-      tab: e.detail.current
-    })
-  },
-  rank(res) {
-    wx.redirectTo({
-      url: '/pages/rank/rank',
-    })
-  },
+  // showModal(title, content) {
+  //   wx.showModal({
+  //     showCancel: false,
+  //     title,
+  //     content: content + ""
+  //   })
+  // },
 
-  async onLoad(options) {
-    const student = (await db.collection("test").where({
-      _openid: '{openid}'
-    }).get()).data[0]
-    if (!student) {
-      wx.redirectTo({
-        url: '/pages/register/register',
-      })
-    } else {
-      wx.setStorageSync('student', student)
-    }
-
-    const list = (await db.collection('test')
+  async one1() {
+    const median = (await oStudents
       .aggregate()
-      .unwind({
-        path: '$target',
-        includeArrayIndex: 'index'
-      })
-      .match({
-        index: 0
-      })
+      // 排名
       .sort({
-        total: -1
+        score: -1
       })
+      // 求总人数
       .group({
-        _id: "$target",
+        _id: null,
+        count: $.sum(1),
         students: $.push({
-          _id: "$_id",
-          total: "$total"
+          score: "$score"
+        })
+      })
+      // 计算中位数
+      .project({
+        _id: 0,
+        // 设中位数下标为index
+        median: $.let({
+          vars: {
+            // 向下取整
+            index: $.floor($.divide(['$count', 2])),
+          },
+          // 找到对应下标的成绩
+          in: $.arrayElemAt(['$students.score', '$$index']),
         }),
       })
-      .end()).list
-    console.log(list);
-    // list.forEach(cInfo => {
-    //   cInfo.students.unshift({
-    //     name: "排名",
-    //     total: "分数"
-    //   })
-    // })
-    this.setData({
-      student,
-      list
-    })
+      .end()).list[0].median
+    console.log(median);
+    // this.showModal("19的成绩中位数是",median)
+  },
 
+  onetow() {
+
+  },
+
+  async tow1() {
+    const res = (await oStudents
+      .aggregate()
+      .sort({
+        score: -1
+      })
+      .limit(10)
+      .end()).list
+    console.log(res);
+  },
+
+  async three1() {
+    const count = (await oStudents
+      .where({
+        "target": [
+          "web前端",
+          "移动应用",
+          "手机游戏"
+        ],
+      }).count())
+    console.log(count);
+  },
+
+  async four1() {
+    const maxScore = (await oStudents
+      .aggregate()
+      .sort({
+        score: -1
+      })
+      .match({
+        gender: "女"
+      })
+      .limit(1)
+      .end()).list[0].score
+    console.log(maxScore);
+  },
+
+  async fives1() {
+    const list = (await oStudents
+      .aggregate()
+      .group({
+        _id: '$gender',
+        count: $.sum(1)
+      })
+      .sort({
+        _id: -1
+      })
+      .end()).list
+    console.log(list[0].count / list[1].count);
+  },
+
+  onLoad() {
   }
 })
